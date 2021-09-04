5ae62e03e6986e3856942438d0e65d2f89752d6a
Erick Solis Mahl <A00904052@itesm.mx>
2021-09-03 17:35:45 -0700

    Initial Commit

---
+++ LerningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LerningGH
+//
+//  Created by Erick Solis Mahl on 03/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LerningGH/LerningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LerningGHApp.swift
+//  LerningGH
+//
+//  Created by Erick Solis Mahl on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct LerningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
